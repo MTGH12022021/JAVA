9261426310afc2221189f09db79aa8e3d8dfdc35
Sawashi <kietle234.cc@gmail.com>
2022-11-29 15:40:36 +0700

    Prototype for login window

---
+++ untitled/src/loginWindow/loginWindow.java
@@ -0,0 +1,25 @@
+package loginWindow;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class loginWindow extends JDialog{
+    private JTextField username;
+    private JTextField passwordTextField;
+    private JButton Login;
+    private JButton signup;
+    private JPanel loginPanel;
+    public loginWindow(JFrame parent){
+        super(parent);
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(450, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        loginWindow loginWindow = new loginWindow(null);
+    }
+}
