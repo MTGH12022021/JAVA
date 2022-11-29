4f059aa9059d50170e9856c966ed428ab44057c5
Sawashi <kietle234.cc@gmail.com>
2022-11-29 19:57:10 +0700

    Prototype of login history of the app

---
+++ untitled/src/loginHistoryWindow/loginHistoryWindow.java
@@ -0,0 +1,35 @@
+package loginHistoryWindow;
+
+import loginWindow.loginWindow;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class loginHistoryWindow extends JDialog{
+    private JPanel historyTablePanel;
+    private JButton menuButton;
+    private JTable loginHistory;
+    public loginHistoryWindow(JFrame parent){
+        super(parent);
+        createTable();
+        setTitle("Login");
+        setContentPane(historyTablePanel);
+        setMinimumSize(new Dimension(450, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public JPanel getRootPanel(){
+        return historyTablePanel;
+    }
+
+    private void createTable() {
+        loginHistory.setModel(new DefaultTableModel(null, new String[]{"Time", "Username", "Nickname"}));
+    }
+    public static void main(String[] args){
+        loginHistoryWindow loginHistoryWindow = new loginHistoryWindow(null);
+    }
+}
