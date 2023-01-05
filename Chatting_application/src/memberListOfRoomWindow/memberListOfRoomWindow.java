956d3d15067dec22f67dcc9d70126d4a163e9796
Sawashi <kietle234.cc@gmail.com>
2023-01-05 21:57:34 +0700

    Add load users for member list of room

--- Chatting_application/src/memberListOfRoomWindow/memberListOfRoomWindow.java
+++ Chatting_application/src/memberListOfRoomWindow/memberListOfRoomWindow.java
@@ -1,52 +1,58 @@
 package memberListOfRoomWindow;
 
-import settingForChatRoomWindow.settingForChatRoomWindow;
-import addANewPersonWindow.addANewPersonWindow;
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class memberListOfRoomWindow extends JFrame{
-    private JButton backButton;
-    private JCheckBox adminCheckBox;
-    private JButton deleteFromRoomButton;
-    private JButton addANewMemberButton;
-    private JPanel memberListOfRoom;
-//    public memberListOfRoomWindow(JFrame parent){
-//        super(parent);
-//        setTitle("Member list of the room");
-//        setContentPane(memberListOfRoom);
-//        setMinimumSize(new Dimension(450, 474));
-//        setModal(true);
-//        setLocationRelativeTo(parent);
-//        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-//        setVisible(true);
-//    }
-public memberListOfRoomWindow(){
-        this.setContentPane(this.memberListOfRoom);
-        this.setSize(450, 474);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setTitle("Member list of the room");
-        this.setVisible(true);
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                new settingForChatRoomWindow();
-            }
-        });
-        addANewMemberButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-                new addANewPersonWindow();
-            }
-        });
+public class memberListOfRoomWindow {
+    public static String[] name = {"Volvo", "BMW", "Ford", "Mazda"};
+    static String url = System.getProperty("user.dir")+"/src/memberListOfRoomWindow/temp_avatar.jpg";
+    public static String[] urls = {url, url, url, url};
+    public JPanel newPersonPanel(String url, String name) throws IOException {
+        JPanel newPanel = new JPanel();
+        newPanel.setLayout(new BorderLayout());
+        BufferedImage myPicture = ImageIO.read(new File(url));
+        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+        JLabel nameLabel = new JLabel(name);
+        newPanel.add(picLabel, BorderLayout.WEST);
+        newPanel.add(nameLabel, BorderLayout.CENTER);
+        return newPanel;
     }
-    public static void main(String[] args){
-        //memberListOfRoomWindow memberListOfRoomWindow = new memberListOfRoomWindow(null);
+    public memberListOfRoomWindow(){
+        JFrame newFrame = new JFrame("Member list");
+        newFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        newFrame.setSize(300, 400);
+        JPanel toolBar = new JPanel();
+        JButton backButton = new JButton("Back");
+        JButton addNewButton = new JButton("Add a person");
+        toolBar.add(backButton);
+        toolBar.add(addNewButton);
+//        JLabel titleLabel = new JLabel("Member list of room");
+        JPanel showUserField = new JPanel();
+        JPanel newPanel = new JPanel();
+        newPanel.setLayout(new BorderLayout());
+        //System.out.println(System.getProperty("user.dir")+"/src/memberListOfRoomWindow/temp_avatar.jpg");
+        JScrollPane scrollableTextArea = new JScrollPane(showUserField);
+        scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        showUserField.setLayout(new GridLayout(4, 1));
+        try {
+            for(int i=0; i<4; i++){
+                showUserField.add(newPersonPanel(urls[i], name[i]));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        newPanel.add(toolBar, BorderLayout.NORTH);
+        //newPanel.add(titleLabel);
+        newPanel.add(scrollableTextArea, BorderLayout.CENTER);
+        newFrame.add(newPanel);
+        newFrame.setVisible(true);
+    }
+    public static void main(String args[]){
         memberListOfRoomWindow memberListOfRoomWindow = new memberListOfRoomWindow();
-
     }
-
 }
